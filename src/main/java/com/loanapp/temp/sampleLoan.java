ec1e89954b5228564b09d6885209f2bce9ba3165
Ryan Roskowski <ryan.roskowski@wilkes.edu>
2019-06-17 22:08:33 -0400

    Initial commit
    Mockup of customer dashboard with static data for now

---
+++ src/main/java/com/loanapp/temp/sampleLoan.java
@@ -0,0 +1,48 @@
+package com.loanapp.temp;
+
+public class sampleLoan{
+	private String loanDate;
+	private String loanStatus;
+	private String loanType;
+	private String loanPeriod;
+	private String loanAmount;
+	public sampleLoan(String loanDate, String loanStatus, String loanType, String loanPeriod, String loanAmount) {
+		super();
+		this.loanDate = loanDate;
+		this.loanStatus = loanStatus;
+		this.loanType = loanType;
+		this.loanPeriod = loanPeriod;
+		this.loanAmount = loanAmount;
+	}
+	public String getLoanDate() {
+		return loanDate;
+	}
+	public void setLoanDate(String loanDate) {
+		this.loanDate = loanDate;
+	}
+	public String getLoanStatus() {
+		return loanStatus;
+	}
+	public void setLoanStatus(String loanStatus) {
+		this.loanStatus = loanStatus;
+	}
+	public String getLoanType() {
+		return loanType;
+	}
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+	public String getLoanPeriod() {
+		return loanPeriod;
+	}
+	public void setLoanPeriod(String loanPeriod) {
+		this.loanPeriod = loanPeriod;
+	}
+	public String getLoanAmount() {
+		return loanAmount;
+	}
+	public void setLoanAmount(String loanAmount) {
+		this.loanAmount = loanAmount;
+	}
+
+}

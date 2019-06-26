bbba1ddac65e05185a891fa44f5f1bfd59feb8cd
Ryan Roskowski <ryan.roskowski@wilkes.edu>
2019-06-25 21:22:32 -0400

    Moved DB queries to dao layer

--- src/main/java/com/loanapp/controller/CustomerDashboardController.java
+++ src/main/java/com/loanapp/controller/CustomerDashboardController.java
@@ -1,31 +1,20 @@
 package com.loanapp.controller;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.InvalidResultSetAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.loanapp.beans.DBProperties;
 import com.loanapp.beans.Loan;
+import com.loanapp.dao.CustomerLoanDao;
 
 @Controller
 public class CustomerDashboardController {
 	
 	@Autowired
-	DBProperties props;
-	
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+	CustomerLoanDao dao;
 	
 	
 	@RequestMapping("/customerDashboard")
@@ -34,46 +23,20 @@ public class CustomerDashboardController {
 		System.out.println(username);
 
 		// get the customerId
-		int customerId = (Integer) jdbcTemplate.queryForObject(
-				"select CUSTOMER_INFO from LOAN_APP_USERS where USER_LOGIN = '" + username + "'", Integer.class);
+		int customerId = dao.getCustomerId(username);
 
 		// get the customers loans
-		List<Loan> loans = jdbcTemplate.query("select * from LOAN_APP_LOANS where CUSTOMER_ID = " + customerId,
-				new RowMapper<Loan>() {
-					public Loan mapRow(ResultSet rs, int rowNum) throws SQLException {
-						Loan loan = new Loan();
-						loan.setLoanID(rs.getInt("LOAN_ID"));
-						loan.setLoanType(rs.getInt("LOAN_TYPE"));
-						loan.setCustomerID(rs.getInt("CUSTOMER_ID"));
-						loan.setAmount(rs.getDouble("AMOUNT"));
-						loan.setStartDate(rs.getString("START_DATE"));
-						loan.setLoanDuration(rs.getInt("LOAN_DURATION"));
-						loan.setInterestRate(rs.getDouble("INTEREST_RATE"));
-						loan.setLoanStatus(rs.getString("LOAN_STATUS"));
-						// loan.setReviewStatus(rs.getString("EMAIL_STATUS"));
-						loan.setDownPayment(rs.getDouble("DOWN_PAYMENT"));
-						return loan;
-					}
-				});
-
+		List<Loan> loans = dao.getLoans(customerId);
+		
 		// get loan types, will use to parse the loan type (int) attribute in the JSP
-		List<String> types = jdbcTemplate.query("select LOAN_TYPE from LOAN_APP_LOAN_TYPES order by LOAN_ID",
-				new RowMapper<String>() {
-					public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-						return rs.getString(1);
-					}
-				});
+		List<String> types = dao.getTypes();
 
 		// get the customers first name
-		String customerFirstName = (String) jdbcTemplate.queryForObject(
-				"select FIRST_NAME from LOAN_APP_USERS where CUSTOMER_INFO = ?", new Object[] { customerId },
-				String.class);
-
+		String customerFirstName = dao.getCustomerFirstName(customerId);
+		
 		// get the customers last name
-		String customerLastName = (String) jdbcTemplate.queryForObject(
-				"select LAST_NAME from LOAN_APP_USERS where CUSTOMER_INFO = ?", new Object[] { customerId },
-				String.class);
-
+		String customerLastName = dao.getCustomerLastName(customerId);
+		
 		model.addAttribute("customerName", customerFirstName + " " + customerLastName);
 		model.addAttribute("loans", loans);
 		model.addAttribute("types", types);
---
+++ src/main/java/com/loanapp/dao/CustomerLoanDao.java
@@ -0,0 +1,70 @@
+package com.loanapp.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.loanapp.beans.DBProperties;
+import com.loanapp.beans.Loan;
+
+@Repository
+public class CustomerLoanDao {
+	
+	@Autowired
+	DBProperties properties;
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public int getCustomerId(String username) {
+		return  (Integer) jdbcTemplate.queryForObject(
+				"select CUSTOMER_INFO from LOAN_APP_USERS where USER_LOGIN = '" + username + "'", Integer.class);
+	}
+	
+	public List<Loan> getLoans(int customerId){
+		return jdbcTemplate.query("select * from LOAN_APP_LOANS where CUSTOMER_ID = " + customerId,
+				new RowMapper<Loan>() {
+			public Loan mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Loan loan = new Loan();
+				loan.setLoanID(rs.getInt("LOAN_ID"));
+				loan.setLoanType(rs.getInt("LOAN_TYPE"));
+				loan.setCustomerID(rs.getInt("CUSTOMER_ID"));
+				loan.setAmount(rs.getDouble("AMOUNT"));
+				loan.setStartDate(rs.getString("START_DATE"));
+				loan.setLoanDuration(rs.getInt("LOAN_DURATION"));
+				loan.setInterestRate(rs.getDouble("INTEREST_RATE"));
+				loan.setLoanStatus(rs.getString("LOAN_STATUS"));
+				// loan.setReviewStatus(rs.getString("EMAIL_STATUS"));
+				loan.setDownPayment(rs.getDouble("DOWN_PAYMENT"));
+				return loan;
+			}
+		});
+	}
+	
+	public String getCustomerFirstName(int customerId) {
+		return  (String) jdbcTemplate.queryForObject(
+				"select FIRST_NAME from LOAN_APP_USERS where CUSTOMER_INFO = ?", new Object[] { customerId },
+				String.class);
+	}
+	
+	public String getCustomerLastName(int customerId) {
+		return (String) jdbcTemplate.queryForObject(
+				"select LAST_NAME from LOAN_APP_USERS where CUSTOMER_INFO = ?", new Object[] { customerId },
+				String.class);
+	}
+	
+	public List<String> getTypes(){
+		return jdbcTemplate.query("select LOAN_TYPE from LOAN_APP_LOAN_TYPES order by LOAN_ID",
+				new RowMapper<String>() {
+			public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return rs.getString(1);
+			}
+		});
+
+	}
+}
